ec90b1ce55037284dad7f044a98b0f002cb151dc
darjow <dariobronders123@hotmail.com>
2023-02-23 18:15:50 +0100

    Json output of all articles of today.

--- src/threads/hlnThread.py
+++ src/threads/hlnThread.py
@@ -3,6 +3,8 @@ from util.volatileDictionary import VolatileDict
 from bs4 import BeautifulSoup
 import requests
 import time
+from datetime import datetime
+
 class HLNThread(AbstractThread):
   
   def __init__(self, volatile_dict:VolatileDict ):
@@ -17,26 +19,54 @@ class HLNThread(AbstractThread):
     self.articles = []
     
     self.guard = []
+    self.headers = {
+      "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3",
+      "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8",
+      "Accept-Encoding": "gzip, deflate, br",
+      "Accept-Language": "nl-NL,nl;q=0.8",
+      "Cache-Control": "max-age=0",
+      "Cookie":"authId=84f9c58f-0da1-4752-a294-74df52a18ec8; authId=84f9c58f-0da1-4752-a294-74df52a18ec8;",
+    }
+    
     
 
 
   def start_scraping(self): 
     self.articles.append(f"{self.base_url}{self.routes[0]}")
     
+    
     while len(self.articles) > 0:
       print(f"{len(self.articles)} to process")
       current = self.articles.pop()
-      
-      response = requests.get(current)
+      response = requests.get(current, headers = self.headers)
       bs = BeautifulSoup(response.text, "html.parser")
       articles = bs.find_all("article")
-      for article in articles:
-        self.add_article(article)
-        title_element = article.find("h2", class_='title')
-        if title_element:
-          title = title_element.text.strip()
-          self.append_to_dict(current, title)
+      time = bs.find_all(name="time", attrs={"class", "article__time"})
       
+      if len(time) == 1:
+        date = time[0]["datetime"]
+        date_format = "%y-%m-%d, %H:%M"
+        try:
+          date_time_obj = datetime.strptime(date, date_format)
+          if date_time_obj.today():
+            self.append_to_dict(self.base_url, current)
+            print("Found an article of today")
+          
+        except(ValueError):
+          print("Article from multiple days ago.")
+            
+      for article in articles:
+        first = article.findChild()
+        if first == None:
+          continue
+        if first.name != "a":
+          continue
+        if any(route in first.get('href', '') for route in self.routes):
+          self.add_article(first["href"])         
+    
+    self.output_dict()
+
+        
       
   def add_article(self, article: str):
     if self.guard.count(article) > 0:
@@ -44,4 +74,7 @@ class HLNThread(AbstractThread):
     
     self.guard.append(article)
     self.articles.append(article)
-    print(f"Found new article: {article}")
+    print(f"Found new article")
+    
+    
+    
