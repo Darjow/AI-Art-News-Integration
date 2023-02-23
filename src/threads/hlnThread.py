6f3179daccc5cc63110135c4c3511edfacdc3537
darjow <dariobronders123@hotmail.com>
2023-02-23 16:33:15 +0100

    Forgot to add newly found articles to the queue

--- src/threads/hlnThread.py
+++ src/threads/hlnThread.py
@@ -16,6 +16,8 @@ class HLNThread(AbstractThread):
     self.base_url = "https://www.hln.be"
     self.articles = []
     
+    self.guard = []
+    
 
 
   def start_scraping(self): 
@@ -29,9 +31,17 @@ class HLNThread(AbstractThread):
       bs = BeautifulSoup(response.text, "html.parser")
       articles = bs.find_all("article")
       for article in articles:
+        self.add_article(article)
         title_element = article.find("h2", class_='title')
         if title_element:
           title = title_element.text.strip()
           self.append_to_dict(current, title)
       
       
+  def add_article(self, article: str):
+    if self.guard.count(article) > 0:
+      return
+    
+    self.guard.append(article)
+    self.articles.append(article)
+    print(f"Found new article: {article}")
