3b85ad7a3fdbad83dc5ee7cddbd02f21d02250aa
darjow <dariobronders123@hotmail.com>
2023-05-26 14:22:07 +0200

    Small changes

--- src/openAI/util/openairequesthandler.py
+++ src/openAI/util/openairequesthandler.py
@@ -6,9 +6,17 @@ class RequestHandler:
     openai.api_key =  config.api_key()
 
   def ask_chatgpt(self, conversation):  
-    response = openai.ChatCompletion.create(model="gpt-3.5-turbo",messages = conversation)
+    response = openai.ChatCompletion.create(
+      model="gpt-3.5-turbo",
+      messages = conversation
+    )    
+    
     generated_text = response["choices"][0].message.content.strip()
     return generated_text
   
   def generate_image(self, prompt):
-    return openai.Image.create(prompt = prompt, n=1, size="1024x1024")["data"][0]["url"]
+    return openai.Image.create(
+      prompt = prompt, 
+      n=1, 
+      size="1024x1024"
+      )["data"][0]["url"]
